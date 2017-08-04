83d6a34ba4a28d8610347af31cdfcac9949e9dd9
Sam Van Hoof <samvanhoof@Sams-MacBook-Pro.local>
2017-08-04 11:16:28 +0200

    added event handlers and content props

--- src/App.js
+++ src/App.js
@@ -1,18 +1,12 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Cookie from './node_modules/components/cookieToaster/CookieToaster';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Cookie title="test" />
       </div>
     );
   }
